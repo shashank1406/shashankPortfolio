24a6ab20648a9f718a6e61345009f5bbe2ec4550
shashank1406 <satu5050@gmail.com>
2022-02-06 16:57:35 +0530

    portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing", {
+    strings: ["", "Backend developer", "Web Developer"],
+    typeSpeed: 150,
+    BackSpeed: 20,
+    loop: true
+})
